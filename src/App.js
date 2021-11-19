14a8b437a82a91925e58449f529762c2af79d093
Onyi (Dre) Anozie <88848554+onyianozie@users.noreply.github.com>
2021-11-19 10:51:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import PieceOfArt from './PieceOfArt';
+// on page load, make a call to the Rijks museum API
+//store the data from the API in state
+//display the results to the page
+// -> image and art piece information
 
 function App() {
+  // piece of state to hold our art pieces
+  const [art, setArt] = useState([])
+
+  //when 
+
+  useEffect (() => {
+    //in here we will call our API using axios
+    const apiKey = '6sJY1L53';
+    axios({
+      url:'https://www.rijksmuseum.nl/api/en/collection',
+      method:'GET',
+      responseType: 'json',
+      params:{
+        key: apiKey,
+        imgonly: true,
+      }
+    })
+    .then(response => {
+      console.log(response.data.artObjects)
+      setArt(response.data.artObjects)
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Look at our art</h1>
+      {
+        art.map(artwork => {
+         return(
+          <PieceOfArt 
+            key={artwork.id}
+            alt={artwork.title}
+            title={artwork.longTitle}
+            imagePath={artwork.webImage.url}
+          />
+         )
+        })
+      }
     </div>
   );
 }
---
+++ src/PieceOfArt.js
@@ -0,0 +1,12 @@
+function PieceOfArt(props){
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <img src={props.imagePath} alt={props.alt}/>
+        </div>
+    )
+}
+
+export default PieceOfArt;
+
+//P
